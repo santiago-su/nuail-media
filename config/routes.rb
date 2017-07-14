7978592ff9188eb984cdbfb9be065e4eeb5eadb5
santiago-su <santiago.su.mo@gmail.com>
2017-07-14 15:31:18 -0500

    Hack to not signup users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_scope :user do
+    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+  end
+
+  devise_for :users, :skip => [:registrations]
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
+
   root to: "pages#home"
   get 'video-service',      to: 'pages#video'
   get 'photo-service',      to: 'pages#photo'
